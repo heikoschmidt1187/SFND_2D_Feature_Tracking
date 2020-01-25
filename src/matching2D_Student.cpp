71000fc56151bc2d3a1d783e47fc45f48818b470
heikoschmidt1187 <github@heikoschmidt.info>
2020-01-25 17:21:51 +0100

    TASK MP.4 - started detector implementation, currently problems with AKAZE

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -39,19 +39,27 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
 {
     // select appropriate descriptor
     cv::Ptr<cv::DescriptorExtractor> extractor;
-    if (descriptorType.compare("BRISK") == 0)
-    {
+    if (descriptorType.compare("BRISK") == 0) {
 
         int threshold = 30;        // FAST/AGAST detection threshold score.
         int octaves = 3;           // detection octaves (use 0 to do single scale)
         float patternScale = 1.0f; // apply this scale to the pattern used for sampling the neighbourhood of a keypoint.
 
         extractor = cv::BRISK::create(threshold, octaves, patternScale);
-    }
-    else
-    {
 
-        //...
+    } else if (descriptorType.compare("BRIEF") == 0) {
+        extractor = cv::xfeatures2d::BriefDescriptorExtractor::create();
+    } else if (descriptorType.compare("ORB") == 0) {
+        extractor = cv::ORB::create();
+    } else if (descriptorType.compare("FREAK") == 0) {
+        extractor = cv::xfeatures2d::FREAK::create();
+    } else if (descriptorType.compare("AKAZE") == 0) {
+        extractor = cv::AKAZE::create();
+    } else if (descriptorType.compare("SIFT") == 0) {
+        extractor = cv::xfeatures2d::SIFT::create();
+    } else {
+        cout << "Unknown descriptor type " << descriptorType << endl;
+        return;
     }
 
     // perform feature description
